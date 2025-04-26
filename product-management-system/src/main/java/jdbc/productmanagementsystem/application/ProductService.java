10eaacf8eb2517f7b07178852531e8b500f6d060
GoGradually <chkun3109@gmail.com>
2025-04-26 15:55:31 +0900

    feat: 상품이 발견되지 않았을 때를 위한 ProductNotFoundException 추가

--- product-management-system/src/main/java/jdbc/productmanagementsystem/application/ProductService.java
+++ product-management-system/src/main/java/jdbc/productmanagementsystem/application/ProductService.java
@@ -1,5 +1,6 @@
 package jdbc.productmanagementsystem.application;
 
+import jdbc.productmanagementsystem.domain.exception.ProductNotFoundException;
 import jdbc.productmanagementsystem.domain.model.product.Product;
 import jdbc.productmanagementsystem.domain.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,26 +17,30 @@ public class ProductService {
         return productRepository.save(product);
     }
     public Product findProductById(Long id){
-        return productRepository.findById(id);
+        return productRepository.findById(id)
+                .orElseThrow(()->new ProductNotFoundException(String.format("Product with id %d not found.", id)));
     }
     public List<Product> findAllProducts(){
         return productRepository.findAll();
     }
 
     public Product changeName(Long id, String name){
-        Product product = productRepository.findById(id);
+        Product product = productRepository.findById(id)
+                .orElseThrow(()->new ProductNotFoundException(String.format("Product with id %s not found",id)));
         product.changeName(name);
         return productRepository.save(product);
     }
 
     public Product changeQuantity(Long id, Long quantity){
-        Product product = productRepository.findById(id);
+        Product product = productRepository.findById(id)
+                .orElseThrow(()->new ProductNotFoundException(String.format("Product with id %s not found",id)));
         product.changeQuantity(quantity);
         return productRepository.save(product);
     }
 
     public Product changePrice(Long id, Long price){
-        Product product = productRepository.findById(id);
+        Product product = productRepository.findById(id)
+                .orElseThrow(()->new ProductNotFoundException(String.format("Product with id %s not found",id)));
         product.changePrice(price);
         return productRepository.save(product);
     }
