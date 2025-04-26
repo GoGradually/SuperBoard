1dbd7a7c5e4a11cf2ab9c6f0328a8ebf49d7d3a3
GoGradually <chkun3109@gmail.com>
2025-04-26 15:55:55 +0900

    feat: ProductRepository 의 단건 조회가 Optional을 반환하도록 변

--- product-management-system/src/main/java/jdbc/productmanagementsystem/domain/repository/ProductRepository.java
+++ product-management-system/src/main/java/jdbc/productmanagementsystem/domain/repository/ProductRepository.java
@@ -3,10 +3,11 @@ package jdbc.productmanagementsystem.domain.repository;
 import jdbc.productmanagementsystem.domain.model.product.Product;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository {
     Product save(Product product);
-    Product findById(Long id);
+    Optional<Product> findById(Long id);
     List<Product> findAll();
     void deleteById(Long id);
 }
