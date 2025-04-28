2dbddf5c01c8ed229a2f76b3adea6b853ce3c628
GoGradually <chkun3109@gmail.com>
2025-04-28 21:20:54 +0900

    feat(CommentDaoJDBC): CommentDaoJDBC 구현체 생성

---
+++ board/src/main/java/jdbc/board/infrastructure/repository/CommentDaoJDBC.java
@@ -0,0 +1,56 @@
+package jdbc.board.infrastructure.repository;
+
+import jdbc.board.domain.board.exception.CommentNotFoundException;
+import jdbc.board.domain.board.model.Comment;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class CommentDaoJDBC {
+    private final NamedParameterJdbcTemplate template;
+
+    public void save(Comment comment) {
+        if (comment.getId() != null) {
+            merge(comment);
+        }
+        insert(comment);
+    }
+
+    public void delete(Comment comment) {
+        String sql = """
+                delete from comment where id = :id
+                """;
+        SqlParameterSource sqlParameterSource = getSqlParameterSource(comment);
+        template.update(sql, sqlParameterSource);
+    }
+
+    private void insert(Comment comment) {
+        String sql = """
+                insert into comment (id, contents, post_id) values (:id, :contents, :post_id)
+                """;
+        SqlParameterSource sqlParameterSource = getSqlParameterSource(comment);
+        template.update(sql, sqlParameterSource);
+    }
+
+    private void merge(Comment comment) {
+        String sql = """
+                update comment set contents=:contents where id=:id
+                """;
+        SqlParameterSource sqlParameterSource = getSqlParameterSource(comment);
+        int updated = template.update(sql, sqlParameterSource);
+        if (updated != 0) {
+            throw new CommentNotFoundException("해당 댓글을 찾을 수 없습니다.");
+        }
+    }
+
+    private static MapSqlParameterSource getSqlParameterSource(Comment comment) {
+        return new MapSqlParameterSource()
+                .addValue("id", comment.getId())
+                .addValue("contents", comment.getContents())
+                .addValue("post_id", comment.getPost().getId());
+    }
+}
