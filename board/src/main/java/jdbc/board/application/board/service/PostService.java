b9b5f0d84a7b2bb993099fd514bfcdb64631db90
GoGradually <chkun3109@gmail.com>
2025-04-28 17:57:42 +0900

    feat(PostService): 게시글 목록을 조회하기 위한 애플리케이션 서비스 메소드 생성

--- board/src/main/java/jdbc/board/application/board/service/PostService.java
+++ board/src/main/java/jdbc/board/application/board/service/PostService.java
@@ -1,18 +1,27 @@
 package jdbc.board.application.board.service;
 
+import jdbc.board.application.board.dto.PostLine;
+import jdbc.board.application.board.repository.PostQueryRepository;
 import jdbc.board.domain.board.exception.PostNotFoundException;
 import jdbc.board.domain.board.model.Post;
 import jdbc.board.domain.board.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
+    private final PostQueryRepository postQueryRepository;
 
     public Post findPostDetails(Long postId) {
         return postRepository.findById(postId).orElseThrow(() -> new PostNotFoundException("게시글을 찾을 수 없습니다."));
+    }
+
+    public List<PostLine> findAllPostLines(int page, int pageSize) {
+        return postQueryRepository.findAllPostLines(page, pageSize);
     }
 }
