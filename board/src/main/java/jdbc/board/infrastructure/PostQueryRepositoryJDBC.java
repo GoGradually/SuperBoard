6f3cad4149bdb7c0f92d72a192b2c966616dc9bb
GoGradually <chkun3109@gmail.com>
2025-04-28 17:57:18 +0900

    feat(PostQueryRepositoryJDBC): QueryRepository의 JDBC 구현체 생성

---
+++ board/src/main/java/jdbc/board/infrastructure/PostQueryRepositoryJDBC.java
@@ -0,0 +1,45 @@
+package jdbc.board.infrastructure;
+
+import jdbc.board.application.board.dto.PostLine;
+import jdbc.board.application.board.repository.PostQueryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class PostQueryRepositoryJDBC implements PostQueryRepository {
+    private final NamedParameterJdbcTemplate template;
+
+    public List<PostLine> findAllPostLines(int page, int pageSize) {
+        String sql = """
+                SELECT
+                    p.id            AS post_id,
+                    p.title         AS post_title,
+                    COUNT(c.id)     AS comment_count
+                FROM
+                    post p
+                LEFT JOIN
+                    comment c
+                  ON p.id = c.post_id
+                GROUP BY
+                    p.id,
+                    p.title
+                ORDER BY p.id DESC
+                LIMIT :pageSize
+                OFFSET :startIndex
+                """;
+        RowMapper<PostLine> postLineRowMapper = new BeanPropertyRowMapper<>(PostLine.class);
+
+        return template.query(sql,
+                new MapSqlParameterSource()
+                        .addValue("pageSize", pageSize)
+                        .addValue("startIndex", page*pageSize),
+                postLineRowMapper);
+    }
+}
