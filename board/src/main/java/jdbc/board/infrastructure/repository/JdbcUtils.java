5b0613efdfb1fe51673071505c1a4ab18c59f079
GoGradually <chkun3109@gmail.com>
2025-05-15 19:50:51 +0900

    feat: id값을 편히 채우기 위한 유틸 클래스 생성

---
+++ board/src/main/java/jdbc/board/infrastructure/repository/JdbcUtils.java
@@ -0,0 +1,27 @@
+package jdbc.board.infrastructure.repository;
+
+import jdbc.board.domain.shared.Id;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Optional;
+
+public class JdbcUtils {
+    public static void fillId(Object object, Object id) {
+        Optional<Field> field = findIdField(object.getClass());
+        if (field.isEmpty()) return;
+        Field idField = field.get();
+        idField.setAccessible(true);
+        try {
+            idField.set(object, id);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Optional<Field> findIdField(Class<?> clazz) {
+        return Arrays.stream(clazz.getDeclaredFields())
+                .filter(idField -> idField.isAnnotationPresent(Id.class))
+                .findFirst();
+    }
+}
