7c278aa1a8d91a6ac69d0c775c39ab386c2a8148
GoGradually <chkun3109@gmail.com>
2025-05-16 10:47:45 +0900

    refactor: 사용하지 않는 setter 제거

--- board/src/main/java/jdbc/board/application/board/dto/PageState.java
+++ board/src/main/java/jdbc/board/application/board/dto/PageState.java
@@ -1,12 +1,12 @@
 package jdbc.board.application.board.dto;
 
 public class PageState {
-    private int currentPage;
-    private int totalPages;
-    private int startPage;
-    private int endPage;
-    private int prevBlockPage;
-    private int nextBlockPage;
+    private final int currentPage;
+    private final int totalPages;
+    private final int startPage;
+    private final int endPage;
+    private final int prevBlockPage;
+    private final int nextBlockPage;
 
     public PageState(int currentPage, int totalPages, int startPage, int endPage, int prevBlockPage, int nextBlockPage) {
         this.currentPage = currentPage;
@@ -21,47 +21,23 @@ public class PageState {
         return currentPage;
     }
 
-    public void setCurrentPage(int currentPage) {
-        this.currentPage = currentPage;
-    }
-
     public int getTotalPages() {
         return totalPages;
-    }
-
-    public void setTotalPages(int totalPages) {
-        this.totalPages = totalPages;
     }
 
     public int getStartPage() {
         return startPage;
     }
 
-    public void setStartPage(int startPage) {
-        this.startPage = startPage;
-    }
-
     public int getEndPage() {
         return endPage;
-    }
-
-    public void setEndPage(int endPage) {
-        this.endPage = endPage;
     }
 
     public int getPrevBlockPage() {
         return prevBlockPage;
     }
 
-    public void setPrevBlockPage(int prevBlockPage) {
-        this.prevBlockPage = prevBlockPage;
-    }
-
     public int getNextBlockPage() {
         return nextBlockPage;
-    }
-
-    public void setNextBlockPage(int nextBlockPage) {
-        this.nextBlockPage = nextBlockPage;
     }
 }
--- board/src/main/java/jdbc/board/application/board/dto/PostLine.java
+++ board/src/main/java/jdbc/board/application/board/dto/PostLine.java
@@ -9,23 +9,11 @@ public class PostLine {
         return postId;
     }
 
-    public void setPostId(Long postId) {
-        this.postId = postId;
-    }
-
     public String getPostTitle() {
         return postTitle;
     }
 
-    public void setPostTitle(String postTitle) {
-        this.postTitle = postTitle;
-    }
-
     public Long getCommentCount() {
         return commentCount;
-    }
-
-    public void setCommentCount(Long commentCount) {
-        this.commentCount = commentCount;
     }
 }
