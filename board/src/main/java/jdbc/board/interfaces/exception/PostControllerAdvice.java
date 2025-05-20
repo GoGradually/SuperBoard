28bf5969074cf0d0aee5e4ec23849530212fd163
GoGradually <chkun3109@gmail.com>
2025-05-20 16:23:18 +0900

    feat: 제목과 내용의 길이 제한 추가

---
+++ board/src/main/java/jdbc/board/interfaces/exception/PostControllerAdvice.java
@@ -0,0 +1,30 @@
+package jdbc.board.interfaces.exception;
+
+import jdbc.board.domain.board.exception.CommentNotFoundException;
+import jdbc.board.domain.board.exception.InvalidContentsException;
+import jdbc.board.domain.board.exception.InvalidTitleException;
+import jdbc.board.domain.board.exception.PostNotFoundException;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class PostControllerAdvice {
+    @ExceptionHandler(value = {InvalidContentsException.class, InvalidTitleException.class})
+    public String handleContentsException(InvalidContentsException e, Model model) {
+        model.addAttribute("errorMessage", e.getMessage());
+        return "400";
+    }
+
+    @ExceptionHandler(CommentNotFoundException.class)
+    public String handleCommentNotFoundException(CommentNotFoundException e, Model model) {
+        model.addAttribute("errorMessage", e.getMessage());
+        return "404";
+    }
+
+    @ExceptionHandler(PostNotFoundException.class)
+    public String handlePostNotFoundException(PostNotFoundException e, Model model) {
+        model.addAttribute("errorMessage", e.getMessage());
+        return "404";
+    }
+}
