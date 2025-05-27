c54d906a4ca3f8d29dad68cca1dfba64a9be80e9
GoGradually <chkun3109@gmail.com>
2025-05-27 22:21:14 +0900

    docs: postLines의 명세를 좀 더 명확하게 변경

--- board/src/main/java/jdbc/board/interfaces/dto/PostsPageResponseDto.java
+++ board/src/main/java/jdbc/board/interfaces/dto/PostsPageResponseDto.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Schema(description = "조회 페이지의 정보 DTO")
 public class PostsPageResponseDto {
-    @Schema(description = "존재하는 내부 댓글을 관리하는 리스트")
+    @Schema(description = "페이지에 표시될 게시글 목록")
     private final List<PostLine> postLines;
     @Schema(description = "현재 페이지의 정보와 페이지 블록 정보를 담은 DTO")
     private final PageState pageState;
