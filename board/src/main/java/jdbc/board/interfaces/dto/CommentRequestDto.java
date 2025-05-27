8e524d1cbbc9f77f0871114526f4fae61ec2fd34
GoGradually <chkun3109@gmail.com>
2025-05-27 20:28:09 +0900

    docs: 길이 제약조건 swagger 스키마에 기록

--- board/src/main/java/jdbc/board/interfaces/dto/CommentRequestDto.java
+++ board/src/main/java/jdbc/board/interfaces/dto/CommentRequestDto.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "생성/변경을 요청하는 댓글의 생성/변경될 내용을 담은 DTO")
 public class CommentRequestDto {
-    @Schema(description = "원하는 댓글의 내용")
+    @Schema(description = "원하는 댓글의 내용", minLength = 1, maxLength = 200)
     private String contents;
 
     public String getContents() {
--- board/src/main/java/jdbc/board/interfaces/dto/PostRequestDto.java
+++ board/src/main/java/jdbc/board/interfaces/dto/PostRequestDto.java
@@ -4,9 +4,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "생성/수정을 원하는 게시글의 정보")
 public class PostRequestDto {
-    @Schema(description = "생성/수정될 게시글의 제목")
+    @Schema(description = "생성/수정될 게시글의 제목", minLength = 1, maxLength = 200)
     private String title;
-    @Schema(description = "생성/수정될 게시글의 내용")
+    @Schema(description = "생성/수정될 게시글의 내용", minLength = 1, maxLength = 400)
     private String contents;
 
     public String getTitle() {
